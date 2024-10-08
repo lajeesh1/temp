b85a1c1bb0aa0ffb5e5bf1ed2f3d2e1f56f48522
Lajeesh <gokulam.9920@gmail.com>
2024-10-08 14:20:29 +0530

    started react

---
+++ src/Create.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div>
+        <input type="text" />
+        <button type='button'>Add</button>
+      
+    </div>
+  )
+}
+
+export default Create
